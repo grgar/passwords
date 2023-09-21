21034efc489d20c156e0899bba76c44ccd9930c7
George Garside <apps@georgegarside.com>
2023-09-21 20:56:36 +0100

    Add navigation links to home

--- ContentView.swift
+++ ContentView.swift
@@ -1,18 +1,56 @@
 import SwiftUI
 
 struct ContentView: View {
-	@State private var path = NavigationPath()
-	
 	var body: some View {
 		NavigationSplitView {
 			List {
-				Text("a")
+				NavigationLink {
+					EmptyView()
+				} label: {
+					LabeledContent {
+						EmptyView()
+					} label: {
+						Text("Password Rules")
+						Text("Rules to generate compatible passwords with websites' particular requirements.")
+					}
+				}
+
+				NavigationLink {
+					EmptyView()
+				} label: {
+					LabeledContent {
+						EmptyView()
+					} label: {
+						Text("Shared Credentials")
+						Text("Groups of websites known to use the same credential backend, which can be used to enhance suggested credentials to sign in to websites.")
+					}
+				}
+
+				NavigationLink {
+					ChangePasswordURLs()
+				} label: {
+					LabeledContent {
+						EmptyView()
+					} label: {
+						Text("Change Password URLs")
+						Text("To drive the adoption of strong passwords, it's useful to be able to take users directly to websites' change password pages.")
+					}
+				}
+
+				NavigationLink {
+					EmptyView()
+				} label: {
+					LabeledContent {
+						EmptyView()
+					} label: {
+						Text("Websites Where 2FA Code is Appended to Password")
+						Text("Some websites use a two-factor authentication scheme where the user must append a generated code to their password when signing in.")
+					}
+				}
 			}
 			.listStyle(.sidebar)
 		} detail: {
-			NavigationStack(path: $path) {
-				Text("b")
-			}
+			EmptyView()
 		}
 	}
 }
