87b59855bb5d9a7f30a7a6a2d3d5b649ab2aa295
George Garside <apps@georgegarside.com>
2023-10-12 16:55:13 +0100

    Simplify sidebar on macOS

--- ContentView.swift
+++ ContentView.swift
@@ -10,64 +10,88 @@ struct ContentView: View {
 					PasswordRules()
 				} label: {
 					Label {
+						#if os(iOS)
 						LabeledContent {
 							EmptyView()
 						} label: {
 							Text("Password Rules")
 							Text("Rules to generate compatible passwords with websites' particular requirements.")
 						}
+						#else
+						Text("Password Rules")
+						#endif
 					} icon: {
 						Image(systemName: "lock.rectangle")
 					}
+					#if os(iOS)
 					.padding(.vertical)
+					#endif
 				}
 
 				NavigationLink {
 					SharedCredentials()
 				} label: {
 					Label {
+						#if os(iOS)
 						LabeledContent {
 							EmptyView()
 						} label: {
 							Text("Shared Credentials")
 							Text("Groups of websites known to use the same credential backend, which can be used to enhance suggested credentials to sign in to websites.")
 						}
+						#else
+						Text("Shared Credentials")
+						#endif
 					} icon: {
 						Image(systemName: "rectangle.on.rectangle.angled")
 					}
+					#if os(iOS)
 					.padding(.vertical)
+					#endif
 				}
 
 				NavigationLink {
 					ChangePasswordURLs()
 				} label: {
 					Label {
+						#if os(iOS)
 						LabeledContent {
 							EmptyView()
 						} label: {
 							Text("Change Password URLs")
 							Text("To drive the adoption of strong passwords, it's useful to be able to take users directly to websites' change password pages.")
 						}
+						#else
+						Text("Change Password URLs")
+						#endif
 					} icon: {
 						Image(systemName: "rectangle.and.pencil.and.ellipsis")
 					}
+					#if os(iOS)
 					.padding(.vertical)
+					#endif
 				}
 
 				NavigationLink {
 					Appended2FA()
 				} label: {
 					Label {
+						#if os(iOS)
 						LabeledContent {
 							EmptyView()
 						} label: {
 							Text("Websites Where 2FA Code is Appended to Password")
 							Text("Some websites use a two-factor authentication scheme where the user must append a generated code to their password when signing in.")
 						}
+						#else
+						Text("Appended 2FA")
+						#endif
 					} icon: {
 						Image(systemName: "123.rectangle")
 					}
+					#if os(iOS)
 					.padding(.vertical)
+					#endif
 				}
 			}
 			.scrollContentBackground(.hidden)
@@ -77,20 +101,24 @@ struct ContentView: View {
 			.listStyle(.sidebar)
 			.navigationTitle(Text("Passwords Inspector"))
 			#if os(iOS)
-			.navigationBarTitleDisplayMode(.inline)
+				.navigationBarTitleDisplayMode(.inline)
+			#else
+				.navigationSplitViewColumnWidth(min: 180, ideal: 180)
 			#endif
-			.toolbar {
-				ToolbarItemGroup(placement: .principal) {
-					Label {
-						Text("Passwords Inspector")
-					} icon: {
-						Image(systemName: "key.fill")
-							.rotationEffect(.radians(-0.3))
+				.toolbar {
+					#if os(iOS)
+					ToolbarItemGroup(placement: .principal) {
+						Label {
+							Text("Passwords Inspector")
+						} icon: {
+							Image(systemName: "key.fill")
+								.rotationEffect(.radians(-0.3))
+						}
+						.labelStyle(.titleAndIcon)
+						.foregroundStyle(.tint, .tertiary)
 					}
-					.labelStyle(.titleAndIcon)
-					.foregroundStyle(.tint, .tertiary)
+					#endif
 				}
-			}
 		} content: {
 			EmptyView()
 		} detail: {
@@ -99,6 +127,12 @@ struct ContentView: View {
 	}
 }
 
+#if os(macOS)
+#Preview(traits: .fixedLayout(width: 960, height: 640)) {
+	ContentView()
+}
+#else
 #Preview {
 	ContentView()
 }
+#endif
