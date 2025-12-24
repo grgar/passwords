affb20198ae0ac31354edb3ae9b8f88ade33d1aa
George Garside <apps@georgegarside.com>
2025-12-24 23:18:41 +0000

    Add preview for rules loading error

--- Rule/PasswordRules.swift
+++ Rule/PasswordRules.swift
@@ -147,3 +147,9 @@ struct PasswordRules: View {
 		EmptyView()
 	}
 }
+
+#Preview("Error") {
+	NavigationStack {
+		PasswordRules(error: DecodingError.typeMismatch(String.self, .init(codingPath: [], debugDescription: "")))
+	}
+}
