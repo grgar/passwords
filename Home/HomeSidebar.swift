e71be67427f311d84b9a61b5d988391aa0df0d02
George Garside <apps@georgegarside.com>
2025-12-24 23:11:51 +0000

    Refactor category info to separate view

--- Home/HomeSidebar.swift
+++ Home/HomeSidebar.swift
@@ -5,71 +5,9 @@ struct HomeSidebar: View {
 
 	var body: some View {
 		List(selection: $selection) {
-			NavigationLink(value: NavigationCategory.rules) {
-				Label {
-					#if os(iOS)
-					LabeledContent {
-						EmptyView()
-					} label: {
-						Text("Password Rules")
-						Text("Rules to generate compatible passwords with websites' particular requirements.")
-					}
-					#else
-					Text("Password Rules")
-					#endif
-				} icon: {
-					Image(systemName: "lock.rectangle")
-				}
-			}
-
-			NavigationLink(value: NavigationCategory.shared) {
-				Label {
-					#if os(iOS)
-					LabeledContent {
-						EmptyView()
-					} label: {
-						Text("Shared Credentials")
-						Text("Groups of websites known to use the same credential backend, which can be used to enhance suggested credentials to sign in to websites.")
-					}
-					#else
-					Text("Shared Credentials")
-					#endif
-				} icon: {
-					Image(systemName: "rectangle.on.rectangle.angled")
-				}
-			}
-
-			NavigationLink(value: NavigationCategory.change) {
-				Label {
-					#if os(iOS)
-					LabeledContent {
-						EmptyView()
-					} label: {
-						Text("Change Password URLs")
-						Text("To drive the adoption of strong passwords, it's useful to be able to take users directly to websites' change password pages.")
-					}
-					#else
-					Text("Change Password URLs")
-					#endif
-				} icon: {
-					Image(systemName: "rectangle.and.pencil.and.ellipsis")
-				}
-			}
-
-			NavigationLink(value: NavigationCategory.appended) {
-				Label {
-					#if os(iOS)
-					LabeledContent {
-						EmptyView()
-					} label: {
-						Text("Websites Where 2FA Code is Appended to Password")
-						Text("Some websites use a two-factor authentication scheme where the user must append a generated code to their password when signing in.")
-					}
-					#else
-					Text("Appended 2FA")
-					#endif
-				} icon: {
-					Image(systemName: "123.rectangle")
+			ForEach(NavigationCategory.allCases, id: \.self) { category in
+				NavigationLink(value: category) {
+					NavigationCategory.LinkLabel(category: category)
 				}
 			}
 		}
--- Home/NavigationCategory.swift
+++ Home/NavigationCategory.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-enum NavigationCategory: View {
+enum NavigationCategory: View, CaseIterable {
 	case rules, shared, change, appended
 
 	var body: some View {
@@ -13,6 +13,66 @@ enum NavigationCategory: View {
 			ChangePasswordURLs()
 		case .appended:
 			Appended2FA()
+		}
+	}
+
+	struct LinkLabel: View {
+		let category: NavigationCategory
+
+		var body: some View {
+			Label {
+				#if os(iOS)
+				LabeledContent {
+					EmptyView()
+				} label: {
+					Text(category.title)
+					Text(category.longSubtitle)
+				}
+				#else
+				Text(category.title)
+				#endif
+			} icon: {
+				Image(systemName: category.icon)
+			}
+		}
+	}
+
+	var title: String {
+		switch self {
+		case .rules:
+			"Password Rules"
+		case .shared:
+			"Shared Credentials"
+		case .change:
+			"Change Password URLs"
+		case .appended:
+			"Appended 2FA"
+		}
+	}
+
+	var longSubtitle: String {
+		switch self {
+		case .rules:
+			"Rules to generate compatible passwords with websites' particular requirements."
+		case .shared:
+			"Groups of websites known to use the same credential backend, which can be used to enhance suggested credentials to sign in to websites."
+		case .change:
+			"To drive the adoption of strong passwords, it's useful to be able to take users directly to websites' change password pages."
+		case .appended:
+			"Some websites use a two-factor authentication scheme where the user must append a generated code to their password when signing in."
+		}
+	}
+
+	var icon: String {
+		switch self {
+		case .rules:
+			"lock.rectangle"
+		case .shared:
+			"rectangle.on.rectangle.angled"
+		case .change:
+			"rectangle.and.pencil.and.ellipsis"
+		case .appended:
+			"123.rectangle"
 		}
 	}
 }
